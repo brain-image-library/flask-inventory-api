9cd4849bf7dccb64dceab44f6cb30efe50706743
icaoberg <icaoberg@psc.edu>
2023-08-23 23:21:29 -0400

    - Refactor hello() function: add redirect to "https://www.brainimagelibrary.org" - Add a new route "/assets" that returns the data from a JSON file - Refactor get() function: add error handling for missing filename argument - Refactor get() function: add redirect to "https://www.brainimagelibrary.org"

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import json
 import urllib.request
-from flask import Flask, request
+from flask import Flask, request, redirect, abort
 
 app = Flask(__name__)
 
@@ -10,14 +10,43 @@ server = "https://download.brainimagelibrary.org/inventory/"
 
 @app.route("/", methods=["GET"])
 def hello():
-    print(f"Hello, World!")
+    return redirect("https://www.brainimagelibrary.org")
 
 
-@app.route("/get", methods=["GET"])
+@app.route("/assets", methods=["GET"])
+def list():
+    # Get the input string from the URL
+    filename = "list"
+
+    # Define the URL where the JSON file is located
+    path = f"{server}{filename}.json"
+    print(path)
+
+    # Check if the JSON file exists at the given URL
+    try:
+        urllib.request.urlopen(path)
+    except urllib.error.HTTPError as e:
+        # The file doesn't exist
+        return "JSON file not found at {}".format(path)
+    else:
+        # The file exists, so download it
+        json_data = urllib.request.urlopen(path).read()
+
+        # load it into a python dict
+        json_dict = json.loads(json_data)
+        data = json_dict
+
+        return data
+
+
+@app.route("/asset", methods=["GET"])
 def get():
     # Get the input string from the URL
     filename = request.args.get("filename")
     query = request.args.get("query")
+
+    if filename is None:
+        abort(404)
 
     # Define the URL where the JSON file is located
     path = f"{server}{filename}.json"
